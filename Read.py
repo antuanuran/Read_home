ffad6347f67cf1c49bc71575a7cc742cc5923df8
test <test@neto.com>
2022-10-24 08:21:51 +0300

    1 Задание

--- Read.py
+++ Read.py
@@ -1,23 +1,40 @@
+
 cook_book = {}
-list_ingr = []
 with open('recipes.txt', 'rt') as file:
     for l in file:
         food_key = l.strip()
         amount = int(file.readline())
+
+        list_ingr = []
         for i in range(amount):
             ingred_str = file.readline()
             name_ingr, count, measure = ingred_str.split(' | ')
             dict_ingred = {'ingredient_name': name_ingr, 'quantity': count, 'measure': measure.strip()}
 
             list_ingr.append(dict_ingred)
-
-        cook_book[f'{food_key}'] = f'{list_ingr}'
-        list_ingr.clear()
+            
+        cook_book[f'{food_key}'] = list_ingr
         food_key = file.readline()
 
 print(cook_book)
 
-# Первый комит
+
+# 2. Функция, которая на вход принимает список блюд из cook_book и количество персон, для кого мы будем готовить
+
+# def get_shop_list_by_dishes (dishes, person_count):
+#     for iter in cook_book:
+#         if (iter == f'{dishes}'):
+#             a = (cook_book [f'{iter}'])
+#             b = list (a)
+
+# get_shop_list_by_dishes('Омлет', 2)
+
+
+"""
+
+
+
+
 
 
 
