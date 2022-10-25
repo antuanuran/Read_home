0b2aa79dd95e3cf9dc5d5ae46e6cac73f057a20d
test <test@neto.com>
2022-10-25 07:46:05 +0300

    2 Задание - итог

--- Read.py
+++ Read.py
@@ -20,16 +20,22 @@ print(cook_book)
 
 # 2. Функция, которая на вход принимает список блюд из cook_book и количество персон, для кого мы будем готовить
 
-def get_shop_list_by_dishes (dishes, person_count):
+def get_shop_list_by_dishes(dishes, person_count):
+    dict_all = {}
+    dict_food = {}
     for iter in cook_book:
         if (iter == f'{dishes}'):
-            a = (cook_book [f'{iter}'])
-            for z in a:
-                z['quantity'] *= person_count
-                print(z)
+            a = (cook_book[f'{iter}'])
+            for res in a:
+                res['quantity'] *= person_count
+                dict_food['measure'] = res['measure']
+                dict_food['quantity'] = res['quantity']
+                ing = res['ingredient_name']
 
+                dict_all[f'{ing}'] = dict_food
+    print(dict_all)
 
-get_shop_list_by_dishes('Фахитос', 10)
+get_shop_list_by_dishes('Омлет', 3)
 
 
 
